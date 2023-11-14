65ea93eb46f0bb40c71d38c3ad63a0e4620c5a7c
Mariya Rashkova <mrashkkova@gmail.com>
2023-11-14 11:52:17 +0200

    add all pages and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Catalogue from "./components/Catalogue/Catalogue";
+import Create from "./components/Create/Create";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+
 function App() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div id="box">
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/catalogue" element={<Catalogue />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
   );
 }
 
